4c89e66d0c357139c6067be004ebf0142cff7a7d
Vipul-26 <vipulsinghssm1@gmail.com>
2022-09-23 12:01:50 +0530

    css changes

--- pages/article/[article].js
+++ pages/article/[article].js
@@ -109,11 +109,28 @@ const Article = ({ article }) => {
                 <title>
                     My Blog
                 </title>
-                <meta
-                    name="description"
-                    content="This website is created using next js, tailwind css & strapi"
+                <link rel="shortcut icon" href="/favicon.png" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon.png"
                 />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="canonical" href="https://blog-gules-one.vercel.app/" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1.0"
+                />
+                <meta name="description" content="This is a blog website created using next js, tailwind css & strapi content management system." />
+                <meta name="author" content="Vipul Singh" />
+                <meta name="keywords" content="blog page, blog website, vipul blog, blog using nextjs" />
+                <meta name="theme-color" content="#fff" />
+                <meta property="og:title" content="My Blog Page" />
+                <meta property="og:description" content="This is a blog website" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://blog-vipul-26.vercel.app/" />
+                <meta property="og:image" content="/V-BlogNew.png" />
+                <meta name="twitter:image" content="/V-BlogNew.png" />
+                <meta name="robots" content="index, follow" />
             </Head>
             <div className="my-3 md:my-4 lg:my-6 xl:my-8 grid lg:grid-cols-3 gap-4 lg:gap-10 xl:gap-12 single-article">
                 <div className="col-span-2">
@@ -145,6 +162,8 @@ const Article = ({ article }) => {
                             src={imgUrl}
                             alt={article.attributes.title}
                             title={article.attributes.title}
+                            width={400}
+                            height={400}
                         />
                         <div className='centerInfo'>
                             <span className='sourceinfo'>
--- pages/category/[category].js
+++ pages/category/[category].js
@@ -26,11 +26,28 @@ const Category = ({ categories, articles, query }) => {
                 <title>
                     My Blog
                 </title>
-                <meta
-                    name="description"
-                    content="This website is created using next js, tailwind css & strapi"
+                <link rel="shortcut icon" href="/favicon.png" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon.png"
                 />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="canonical" href="https://blog-gules-one.vercel.app/" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1.0"
+                />
+                <meta name="description" content="This is a blog website created using next js, tailwind css & strapi content management system." />
+                <meta name="author" content="Vipul Singh" />
+                <meta name="keywords" content="blog page, blog website, vipul blog, blog using nextjs" />
+                <meta name="theme-color" content="#fff" />
+                <meta property="og:title" content="My Blog Page" />
+                <meta property="og:description" content="This is a blog website" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://blog-vipul-26.vercel.app/" />
+                <meta property="og:image" content="/V-BlogNew.png" />
+                <meta name="twitter:image" content="/V-BlogNew.png" />
+                <meta name="robots" content="index, follow" />
             </Head>
             <Tabs
                 categories={categories.items}
@@ -43,7 +60,7 @@ const Category = ({ categories, articles, query }) => {
                 :
                 <>
                     <ArticleList articles={articles.items} isArtByCategory={true} />
-                    {!(query.search) && <Pagination page={page} pageCount={pageCount} redirectUrl={`/category/${categorySlug}`}/>}
+                    {!(query.search) && <Pagination page={page} pageCount={pageCount} redirectUrl={`/category/${categorySlug}`} />}
                 </>
             }
         </>
