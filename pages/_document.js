82402ee6fefe42538c25e6211688e4cd9184401f
Vipul-26 <vipulsinghssm1@gmail.com>
2022-09-23 16:36:33 +0530

    favicon fix

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,33 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    <title>
+                        My Blog
+                    </title>
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/favicon.ico"
+                    />
+                    <link rel="canonical" href="https://blog-gules-one.vercel.app/" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width,initial-scale=1.0"
+                    />
+                    <meta name="description" content="This is a blog website created using next js, tailwind css & strapi content management system." />
+                    <meta name="author" content="Vipul Singh" />
+                    <meta name="keywords" content="blog page, blog website, vipul blog, blog using nextjs" />
+                    <meta name="theme-color" content="#fff" />
+                    <meta property="og:title" content="My Blog Page" />
+                    <meta property="og:description" content="This is a blog website" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://blog-vipul-26.vercel.app/" />
+                    <meta property="og:image" content="/V-BlogNewest.png" />
+                    <meta name="twitter:image" content="/V-BlogNewest.png" />
+                    <meta name="robots" content="index, follow" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
--- pages/article/[article].js
+++ pages/article/[article].js
@@ -106,33 +106,6 @@ const Article = ({ article }) => {
 
     return (
         <div>
-            <Head>
-                <title>
-                    My Blog
-                </title>
-                <link rel="shortcut icon" href="/favicon.png" />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/favicon.png"
-                />
-                <link rel="canonical" href="https://blog-gules-one.vercel.app/" />
-                <meta
-                    name="viewport"
-                    content="width=device-width,initial-scale=1.0"
-                />
-                <meta name="description" content="This is a blog website created using next js, tailwind css & strapi content management system." />
-                <meta name="author" content="Vipul Singh" />
-                <meta name="keywords" content="blog page, blog website, vipul blog, blog using nextjs" />
-                <meta name="theme-color" content="#fff" />
-                <meta property="og:title" content="My Blog Page" />
-                <meta property="og:description" content="This is a blog website" />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://blog-vipul-26.vercel.app/" />
-                <meta property="og:image" content="/V-BlogNewest.png" />
-                <meta name="twitter:image" content="/V-BlogNewest.png" />
-                <meta name="robots" content="index, follow" />
-            </Head>
             <div className="my-3 md:my-4 lg:my-6 xl:my-8 grid lg:grid-cols-3 gap-4 lg:gap-10 xl:gap-12 single-article">
                 <div className="col-span-2">
                     <h2 className="text-xl font-bold py-1">
